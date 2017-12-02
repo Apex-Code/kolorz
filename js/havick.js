fc75689417210e9b59fbed1098fe711eae1bbfbe
Apex-Code <upstaterounder@gmail.com>
2017-12-02 16:27:06 -0600

    fix padding issue on #message, write function to randomly choose color from array

--- js/havick.js
+++ js/havick.js
@@ -10,9 +10,11 @@ var colors = [
     "rgb(112, 255, 255)"
 ];
 
+var range = colors.length;
 var squares = document.getElementsByClassName("square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay=document.getElementById("colorDisplay");
+var message = document.getElementById("message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,11 +23,26 @@ for(i=0; i<squares.length; i++){
     squares[i].addEventListener("click", function(){
     var clickedColor = this.style.backgroundColor;
         if(clickedColor === pickedColor){
-            alert("correct!!");
+            message.textContent = "Correct!";
+            message.style.color="white";
+            changeColor(pickedColor);
+
+
         } else {
             this.style.backgroundColor = "#232323";
+            message.textContent = "Wrong, Try again!";
+            message.style.color="white";
         }
     });
 }
 
+function changeColor(color){
+    for(var i=0; i<squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
 
+function pickColor(){
+    var rnd = Math.floor(Math.random() * range + 1);
+    return colors[rnd];
+}
