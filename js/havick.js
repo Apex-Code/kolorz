55352761e8716b0215a725e06ad96f433de5f444
Apex-Code <upstaterounder@gmail.com>
2017-12-02 15:47:43 -0600

    start adding js logic

--- js/havick.js
+++ js/havick.js
@@ -1,1 +1,31 @@
-// alert("Connected!!");
+// Built by Havick
+// Last modification:12/02/2017
+
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(112, 255, 255)"
+];
+
+var squares = document.getElementsByClassName("square");
+var pickedColor = colors[3];
+var colorDisplay=document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(i=0; i<squares.length; i++){
+    squares[i].style.backgroundColor=colors[i];
+    squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+        if(clickedColor === pickedColor){
+            alert("correct!!");
+        } else {
+            this.style.backgroundColor = "#232323";
+        }
+    });
+}
+
+
